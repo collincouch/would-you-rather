37b29b2527db55a7ccbb9d89ef4d8faa3fdb71cc
collincouch <collindcouch@gmail.com>
2018-11-20 09:54:38 -0700

    modified quesitons reducer

--- src/reducers/questions.js
+++ src/reducers/questions.js
@@ -18,7 +18,18 @@ import { RECEIVE_QUESTIONS, SAVE_QUESTION_ANSWER  } from '../actions/questions'
     					? state[action.qid][action.answer].votes((uid)=>uid!==action.authedUser)
     					: state[action.qid][action.answer].votes.concat([action.authedUser])
     			}
-    		}
+    		},
+
+    		//I need to change state of users  here too don't I to add the answer to the users?
+    		//
+	        //[action.authedUser]: {
+	        //  ...state[action.authedUser],
+	        //  answers:
+	        //  	{
+	        // ...state[action.authedUser].answers,
+	        //    [action.qid]: action.answer
+	        //  }
+	        //}
 
 
       
